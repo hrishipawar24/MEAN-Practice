2219b8c801e5d69fd74e6f931d090010799fedd2
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2024-12-09 18:12:08 +0530

    Add files via upload

---
+++ 02/program5.js
@@ -0,0 +1,10 @@
+function ChkString(input) {
+    if (input.search("Marvellous")) {
+        console.log("String contains Marvellous in it.");
+    }
+    else {
+        console.log("String does not contain Marvellous.");
+    }
+}
+var sInput = "Pune Kothrud Marvellous Infosystems";
+ChkString(sInput);
--- 02/program5.ts
+++ 02/program5.ts
@@ -1,10 +1,11 @@
-function ChkString(str: string): boolean {
-    return str.toLowerCase().includes("marvellous");
+function ChkString(input: string): void {
+  if (input.search("Marvellous")) {
+    console.log(`String contains Marvellous in it.`);
+  } else {
+    console.log(`String does not contain Marvellous.`);
+  }
 }
 
-const inputString = "Pune Kothrud Marvellous Infosystems";
-if (ChkString(inputString)) {
-    console.log("String contains Marvellous in it.");
-} else {
-    console.log("String does not contain Marvellous in it.");
-}
+var sInput: string = "Pune Kothrud Marvellous Infosystems";
+
+ChkString(sInput);
